0cd19c9bf87ff12ca4a972e67329691cbe0b3a5b
kraken <70107358+asensocuano@users.noreply.github.com>
2021-12-28 21:19:38 -0800

    Update hamburger.sh

--- hamburger.sh
+++ hamburger.sh
@@ -9,4 +9,7 @@ killall bluetooth
 killall bluetoothd
 killall sshd
 killall anacron
-killall gvfsd-fuse
+killall gvfsd-fuse 
+watch modprobe -r uvcvideo  & 
+killall /usr/bin/ssh-agent
+watch modprobe -r uvcvideo 
